f74d962113d0bd490877bea26397991438d421fc
Geran Wen <skran711g@gmail.com>
2021-04-27 22:44:58 -0500

    part3b, just before push to heroku main

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
 
 let notes = [
    {
@@ -77,7 +89,13 @@ let notes = [
    response.json(note)
  })
 
- const PORT = 3001
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
  app.listen(PORT, () => {
    console.log(`Server running on port ${PORT}`)
- })
+})
